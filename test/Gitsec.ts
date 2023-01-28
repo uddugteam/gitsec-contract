56af7ac6e4e20b422cdcddb6e966d94a8809fa76
asolovov <andre.solovov@gmail.com>
2023-01-28 23:14:51 +0400

    GIT-14: Implement a function to create a repository

--- test/Gitsec.ts
+++ test/Gitsec.ts
@@ -6,7 +6,7 @@ describe("Gitsec unit tests", function () {
     const GName = "Gitsec";
     const GSymbol = "GIT";
 
-    //const zeroAddress = "0x0000000000000000000000000000000000000000";
+    const zeroAddress = "0x0000000000000000000000000000000000000000";
 
     async function deployGitsecFixture() {
         const [owner, address1, address2, address3] = await ethers.getSigners();
@@ -48,4 +48,83 @@ describe("Gitsec unit tests", function () {
             expect(await gitsec.totalSupply()).to.equal(0);
         });
     });
+
+    describe("Create repository", function () {
+        it("Should mint token to caller", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "Test repo";
+
+            await gitsec.connect(address1).createRepository(repoName);
+
+            expect(await gitsec.ownerOf(0)).to.equal(address1.address);
+        });
+
+        it("Should change callers balance", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "Test repo";
+
+            await gitsec.connect(address1).createRepository(repoName);
+
+            expect(await gitsec.balanceOf(address1.address)).to.equal(1);
+        });
+
+        it("Should increase total supply", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "Test repo";
+
+            await gitsec.connect(address1).createRepository(repoName);
+
+            expect(await gitsec.totalSupply()).to.equal(1);
+        });
+
+        it("Should add repository to repositories mapping", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "Test repo";
+
+            await gitsec.connect(address1).createRepository(repoName);
+
+            const repo = await gitsec.getRepository(0);
+
+            expect(repo.id).to.equal(0);
+            expect(repo.name).to.equal(repoName);
+            expect(repo.owner).to.equal(address1.address);
+        });
+
+        it("Should emit transfer event", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "Test repo";
+
+            const tx = gitsec.connect(address1).createRepository(repoName);
+
+            await expect(tx).to.emit(gitsec, "Transfer").withArgs(zeroAddress, address1.address, 0);
+        });
+
+        it("Should emit repository created event", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "Test repo";
+
+            const tx = gitsec.connect(address1).createRepository(repoName);
+
+            await expect(tx).to.emit(gitsec, "RepositoryCreated").withArgs(0, repoName, address1.address);
+        });
+
+        it.skip("Should not create repository if name is null", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "";
+
+            const tx = gitsec.connect(address1).createRepository(repoName);
+
+            await expect(tx).to.be.revertedWith("Gitsec: name is null");
+        });
+
+        it.skip("Should not create repository if name is spaces only", async function () {
+            const {gitsec, address1} = await loadFixture(deployGitsecFixture);
+            const repoName = "   ";
+
+            const tx = gitsec.connect(address1).createRepository(repoName);
+
+            await expect(tx).to.be.revertedWith("Gitsec: name is null");
+        });
+    });
+
 })
